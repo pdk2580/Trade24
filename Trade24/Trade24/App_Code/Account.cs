0cb0032950c1204029b4748ffe921df17fe4b3ae
Alan <lan_rock@Live.com>
2016-03-13 10:38:49 +0800

    #Add model class

--- Trade24/Trade24/App_Code/Account.cs
+++ Trade24/Trade24/App_Code/Account.cs
@@ -7,5 +7,18 @@ namespace Trade24.App_Code
 {
     public class Account
     {
+        public string email { get; set; }
+        public string name { get; set; }
+        public string companyName { get; set; }
+        public DateTime createdAt { get; set; }
+        public string password { get; set; }
+        public int countryID { get; set; }
+        public int cityID { get; set; }
+        public string salt { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public bool isVerified { get; set; }
+        public bool isPublicProfile { get; set; }
+        public string lang { get; set; }
     }
 }
