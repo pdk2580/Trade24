2c9ce098a492e9d01ff322d360054276e8badbba
Park <pdk2580@gmail.com>
2016-03-15 12:46:31 +0800

    Added DAL

---
+++ Trade24/Trade24.DAL/CityDAL.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using ObjectDumper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trade24.BO;
+
+namespace Trade24.DAL
+{
+    public class CityDAL
+    {
+        public IEnumerable<CityBO> GetAllCities()
+        {
+            IEnumerable<CityBO> cities;
+
+            using (var sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["trade24"].ConnectionString))
+            {
+                sqlConnection.Open();
+
+                string query = "SELECT * FROM Cities";
+                cities = sqlConnection.Query<CityBO>(query);
+            }
+
+            return cities;
+        }
+    }
+}
