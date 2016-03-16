0be3caa8f17f0f2ef6281e785462b71c953b5af6
Alan <lan_rock@Live.com>
2016-03-16 13:19:01 +0800

    #Added Master-Page (Testing)

---
+++ Trade24/Trade24/Layout/Site.Public.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Trade24.Layout
+{
+    public partial class Site_Public : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
